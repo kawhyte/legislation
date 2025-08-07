4369ae107b09528a8cb724131a37f220885f4ab3
Kenny Whyte <kawhyte@gmail.com>
2025-08-07 10:31:57 -0700

    Updated grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => {
 					selectedJurisdiction={selectedJurisdiction}
 					setSelectedJurisdiction={setSelectedJurisdiction}
 				/>
-			<main className='max-w-[100rem] mx-auto px-4 py-6'>
+			<main className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6'>
 
 				{/* <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mb-6'>
 					<StatCard
